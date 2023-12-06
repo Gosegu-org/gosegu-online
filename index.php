fa1cfcf3a57b1e32a74aa56d18a180f7f0684429
sunhaklee <105528907+sunbaklee@users.noreply.github.com>
2023-12-06 15:57:18 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> 
     <script type="text/javascript">   
       $(document).ready( function() {
       $("#navbar").load("header/header.html"); 
