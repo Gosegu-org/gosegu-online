73f21232c2be27d5b2ff865483c559b5dd7b2f11
donggeun-yu <93115530+dongdong8343@users.noreply.github.com>
2023-12-09 04:51:37 +0900

    리다이렉트 수정 중

--- write/write.php
+++ write/write.php
@@ -12,10 +12,6 @@
   </head>
   <body>
     <?php require_once("../header.php"); ?>
-
-
-
-
     <nav class="navbar">
       <a href="index.html"
         ><img class="logo" src="../Assets/logo.png" alt="로고"
@@ -54,23 +50,40 @@
         ?>
            
      <form id="postForm" action="http://api.gosegu.online:4029/posts/create.php" method="post">
-        <h2 class="text_title">글 작성</h2>
-        <input type="hidden" id="user_id" name="user_id" value="<?php echo $user_id; ?>">
-        <select>
-          <?php foreach ($responseData as $item) : ?>
-             <option value="<?php echo $item['category_id']; ?>">
-                 <?php echo $item['category_name']; ?>
-           </option>
-           <?php endforeach; ?>
-        </select>
-        <label class="text_Title" for="post_title">글 제목</label><br>
-        <input type="text" id="post_title" name="post_title"><br><br>
-        
-        <label for="post_contents">글 내용</label><br>
-        <textarea id="post_contents" name="post_contents" rows="4" cols="50"></textarea><br><br>
-        
-        <input type="submit" value="글 등록">
-     </form>
+    <h2 class="text_title">글 작성</h2>
+    <input type="hidden" id="user_id" name="user_id" value="<?php echo $user_id; ?>">
+    <select id="categorySelect" name="category_id"> <!-- 이름을 category_id로 변경 -->
+        <?php foreach ($responseData as $item) : ?>
+            <option value="<?php echo $item['category_id']; ?>">
+                <?php echo $item['category_name']; ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <label class="text_Title" for="post_title">글 제목</label><br>
+    <input type="text" id="post_title" name="post_title"><br><br>
+    
+    <label for="post_contents">글 내용</label><br>
+    <textarea id="post_contents" name="post_contents" rows="4" cols="50"></textarea><br><br>
+    
+    <input type="submit" value="글 등록" onclick="sendFormData()">
+</form>
+
+<script>
+    function sendFormData() {
+        // 선택된 카테고리 값 가져오기
+        var categorySelect = document.getElementById("categorySelect");
+        var selectedCategoryId = categorySelect.options[categorySelect.selectedIndex].value;
+
+        // 카테고리 값 설정
+        document.getElementById("category_id").value = selectedCategoryId;
+
+        // 폼 제출
+        document.getElementById("postForm").submit();
+
+        // 페이지를 다른 경로로 이동
+        window.location.href = "/"; // Change the URL to your desired path
+    }
+</script>
       </div>
     </div>
     <footer class="footer"></footer>
