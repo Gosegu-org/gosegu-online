e71b193b4ca0c3f9a1fb0620de5131daa54fcbc4
sunhaklee <105528907+sunbaklee@users.noreply.github.com>
2023-12-09 03:15:56 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="ko">
   <head>
+    <?php if (isset($_SESSION) === false){session_start();} ?>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>gosegu online</title>
@@ -14,9 +15,9 @@
         <a href="/about/about.php">About</a>
         <a href="/Member/member.php">Members</a>
         <?php
-        session_start();
           if (isset($_SESSION['user_id'])===true) {
-            echo '<a href="/user/logout.php" class="login-button">로그아웃</a>';
+            echo '<a href="/user/my_account.php">내 정보</a>
+              <a href="/user/logout.php">로그아웃</a>';
           } else {
             // 로그인 되어 있지 않다면
             echo '<a href="/user/login.php" class="login-button">로그인</a>';
