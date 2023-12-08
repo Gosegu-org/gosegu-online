5ef3fc578507fb9b897ca841f4a1ac8c2ca05b99
sunhaklee <105528907+sunbaklee@users.noreply.github.com>
2023-12-08 21:40:30 +0900

    에러코드 간결화

--- models/user/user_create.php
+++ models/user/user_create.php
@@ -8,12 +8,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $api_endpoint = "api.gosegu.online:4029/users/register.php";
 
-    $hashed_pw = password_hash($user_pw, PASSWORD_DEFAULT);
-    
     $data = array(
         'user_id' => $user_id,
         'user_name' => $user_name,
-        'user_pw' => $hashed_pw,
+        'user_pw' => $user_pw,
         'user_email' => $user_email,
         'user_comment' => $user_comment
     );
@@ -28,23 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
     if ($httpCode == 200) {
-        $responseData = json_decode($response, true);
-
-        if ($responseData && isset($responseData['code'])) {
-            $code = $responseData['code'];
-
-            if ($code == 201) {
-                echo '<script>alert("회원가입이 완료되었습니다.");</script>';
-            } elseif ($code == 409) {
-                echo '<script>alert("ID 혹은 이메일이 중복입니다.");</script>';
-            } elseif ($code == 403) {
-                echo '<script>alert("값이 비어있습니다.");</script>';
-            } else {
-                echo '<script>alert("회원가입에 실패했습니다. 다시 시도해주세요.");</script>';
-            }
-        } else {
-            echo '<script>alert("서버 응답 형식 오류");</script>';
-        }
+        echo $response;
     } elseif ($httpCode == 404) {
         echo '<script>alert("서버를 찾을 수 없습니다.");</script>';
     } else {
