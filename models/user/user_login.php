1ad4abaea00fa35d70827210a70aefbfc7e73d30
sunhaklee <105528907+sunbaklee@users.noreply.github.com>
2023-12-08 21:40:56 +0900

    에러코드 간결화

--- models/user/user_login.php
+++ models/user/user_login.php
@@ -5,11 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $api_endpoint = "api.gosegu.online:4029/users/login.php";
 
-    $hashed_pw = password_hash($user_pw, PASSWORD_DEFAULT);
-
     $data = array(
         'user_id' => $user_id,
-        'user_pw' => $hashed_pw,
+        'user_pw' => $user_pw,
     );
 
     $ch = curl_init($api_endpoint);
@@ -22,28 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
     if ($httpCode == 200) {
-        $responseData = json_decode($response, true);
-
-        if ($responseData && isset($responseData['code'])) {
-            $code = $responseData['code'];
-
-            if ($code == 201) {
-                session_start();
-    $_SESSION['user_id'] = $user_id;
-    $_SESSION['user_pw'] = $user_pw;
-    header("Location: /index.php");
-    exit();
-
-            } elseif ($code == 403) {
-                echo '<script>alert("아이디 또는 비밀번호가 올바르지 않습니다.");</script>';
-            } elseif ($code == 400) {
-                echo '<script>alert("아이디 또는 비밀번호 값이 없습니다.");</script>';
-            } else {
-                echo '<script>alert("로그인에 실패했습니다. 다시 시도해주세요.");</script>';
-            }
-        } else {
-            echo '<script>alert("서버 응답 형식 오류");</script>';
-        }
+        echo $response;
     } elseif ($httpCode == 404) {
         echo '<script>alert("서버를 찾을 수 없습니다.");</script>';
     } else {
