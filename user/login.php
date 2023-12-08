cd5a58c33dc50bf6d4f7bda23770ec96e54415a1
sunhaklee <105528907+sunbaklee@users.noreply.github.com>
2023-12-08 17:18:27 +0900

    Create login.php

---
+++ user/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <title>로그인</title>
+    <link rel="stylesheet" href="/user/login.css">
+    <!-- You'll link your CSS file here -->
+    <script>
+        // Your JavaScript functions can go here
+        function goToIndex() {
+            window.location.href = '/index.php';
+        }
+
+        function validateLoginForm() {
+            // Validation logic can be added here
+            // ...
+        }
+    </script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="login-box">
+            <h2>로그인</h2>
+            <span class="close-button" onclick="goToIndex()">&times;</span>
+            <p class="login-message">아래 정보로 로그인하세요</p>
+            <form action="/models/user/언젠가는 추가.php" method="post" onsubmit="return validateLoginForm()">
+                <label for="login_id">사용자 ID</label>
+                <input type="text" id="login_id" name="login_id" required><br>
+
+                <label for="login_pw">비밀번호</label>
+                <input type="password" id="login_pw" name="login_pw" required><br><br>
+
+                <input type="submit" value="로그인">
+            </form>
+            <p class="signup-message">계정이 없으신가요? <a href="/user/register.php">회원가입</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
