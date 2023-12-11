7b16ccadf0088185e84bf66dc66f06c196601070
sunhaklee <105528907+sunbaklee@users.noreply.github.com>
2023-12-12 02:21:56 +0900

    유저 정보창 수정

--- user/edit_profile.php
+++ user/edit_profile.php
@@ -1,7 +1,25 @@
 <!DOCTYPE html>
 <html lang="ko">
 <head>
-    <?php session_start(); ?>
+    <?php session_start(); 
+
+      if (isset($_GET['user_id'])) {
+          $user_id = $_GET['user_id'];
+      
+          $_SESSION["user_id"] = $user_id;
+      } else {
+          $user_id = $_SESSION["user_id"];
+      }
+      
+      $api_url = 'http://api.gosegu.online:4029/users/info.php?user_id=' . $user_id;
+      
+      $curl = curl_init($api_url);
+      
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      $response = curl_exec($curl);
+      $user_info = json_decode($response, true);
+      curl_close($curl);
+    ?>
     <meta charset="UTF-8">
     <title>개인정보 수정</title>
     <link rel="stylesheet" href="/user/edit_profile.css">
@@ -29,16 +47,16 @@
             <p class="signup-message">수정하시려면 아래 정보를 입력하세요</p>
             <form action="/models/user/user_update.php" method="post">
                 <label for="user_name">사용자 이름</label>
-                <input type="text" id="user_name" name="user_name"><br>
+                <input type="text" id="user_name" name="user_name" value="<?php echo $user_info['user_name']; ?>" required><br>
                 
                 <label for="user_pw">비밀번호</label>
-                <input type="password" id="user_pw" name="user_pw"><br>
+                <input type="password" id="user_pw" name="user_pw" required><br>
                 
                 <label for="user_email">이메일</label>
-                <input type="email" id="user_email" name="user_email"><br>
+                <input type="email" id="user_email" name="user_email" value="<?php echo $user_info['user_email']; ?>" required><br>
                 
                 <label for="user_comment">코멘트</label>
-                <input type="comment" id="user_comment" name="user_comment"><br><br>
+                <input type="comment" id="user_comment" name="user_comment" value="<?php echo $user_info['user_comment']; ?>"><br><br>
                 
                 <input type="submit" value="수정하기">
             </form>
