99afd18daa2bc674afa9d5ca3fd99a85ea3ca294
sunhaklee <105528907+sunbaklee@users.noreply.github.com>
2023-12-26 13:05:46 +0900

    상대경로수정

--- index.php
+++ index.php
@@ -5,22 +5,22 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>gosegu online</title>
-    <link rel="stylesheet" href="/index.css" />
+    <link rel="stylesheet" href="index.css" />
   </head>
   <body>
     <nav class="navbar">
-      <a href="/index.php"><img class="logo" src="/Assets/logo.png" alt="로고" /></a>
+      <a href="index.php"><img class="logo" src="Assets/logo.png" alt="로고" /></a>
       <div class="navbar-links">
-        <a href="/Forum/forum.php">Forum</a>
-        <a href="/about/about.php">About</a>
-        <a href="/Member/member.php">Members</a>
+        <a href="Forum/forum.php">Forum</a>
+        <a href="about/about.php">About</a>
+        <a href="Member/member.php">Members</a>
         <?php
           if (isset($_SESSION['user_id'])===true) {
             echo '<a href="/user/my_account.php">내 정보</a>
-              <a href="/user/logout.php">로그아웃</a>';
+              <a href="user/logout.php">로그아웃</a>';
           } else {
             // 로그인 되어 있지 않다면
-            echo '<a href="/user/login.php" class="login-button">로그인</a>';
+            echo '<a href="user/login.php" class="login-button">로그인</a>';
           } 
         ?>
       </div>
